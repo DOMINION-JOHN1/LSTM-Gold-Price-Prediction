1abac41134bb548f86f70fc308f2ba194b2421c4
DOMINION JOHN <119686378+DOMINION-JOHN1@users.noreply.github.com>
2023-11-20 15:25:02 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,6 +9,23 @@ from PIL import Image
 model = keras.models.load_model('best_model (1).h5')
 scaler = StandardScaler()
 
+# Decorate the prediction function with tf.function
+@tf.function(reduce_retracing=True)
+def predict_price(Open, High, Low, Close, Volume, Month_Num, Day_of_Week_Num):
+    # Use the loaded model to make predictions
+    input_data = np.array([[Open, High, Low, Close, Volume, Month_Num, Day_of_Week_Num]])
+    column_names = ["Open", "High", "Low", "Close", "Volume", "Month_Num", "Day_of_Week_Num"]
+    # Creating a DataFrame
+    input_data = pd.DataFrame(data=input_data, columns=column_names)
+    input_data = scaler.fit_transform(input_data)
+    input_data = input_data.reshape(input_data.shape[0], 1, 7)
+    prediction = model.predict(input_data)
+
+    # Calculate the next close (assuming it's the first output in the prediction)
+    next_close = prediction[0][0]
+
+    return next_close
+
 # Streamlit app
 def main():
     st.title('Gold Price Predictor')
@@ -29,17 +46,8 @@ def main():
     Day_of_Week_Num = st.sidebar.number_input('Enter the Day of the Week (1=Monday, 7=Sunday):', min_value=1, max_value=7)
 
     if st.sidebar.button('Predict'):
-        # Use the loaded model to make predictions
-        input_data = np.array([[Open, High, Low, Close, Volume, Month_Num, Day_of_Week_Num]])
-        column_names = ["Open", "High", "Low", "Close", "Volume", "Month_Num", "Day_of_Week_Num"]
-        # Creating a DataFrame
-        input_data = pd.DataFrame(data=input_data, columns=column_names)
-        input_data = scaler.fit_transform(input_data)
-        input_data = input_data.reshape(input_data.shape[0], 1, 7)
-        prediction = model.predict(input_data)
-
-        # Calculate the next close (assuming it's the first output in the prediction)
-        next_close = prediction[0][0]
+        # Call the decorated function
+        next_close = predict_price(Open, High, Low, Close, Volume, Month_Num, Day_of_Week_Num)
 
         st.write(f'Predicted Next Close Price: {next_close:.2f}')
 
